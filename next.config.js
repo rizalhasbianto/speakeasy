cf55a3391fe4f9aefab6ad45c7320588c6ac96c0
rizalhasbianto <rizalhasbianto@gmail.com>
2023-01-23 13:41:51 +0700

    add netlifycms

--- next.config.js
+++ next.config.js
@@ -1,6 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  webpack: (cfg) => {
+        cfg.module.rules.push(
+            {
+                test: /\.md$/,
+                loader: 'frontmatter-markdown-loader',
+            }
+        )
+        return cfg;
+    }
 }
 
 module.exports = nextConfig
