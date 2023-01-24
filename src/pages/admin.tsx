706b1e226aa315f595602a6a9060edd96ad3fa04
rizalhasbianto <rizalhasbianto@gmail.com>
2023-01-24 10:57:23 +0700

    disable no sync eslint

--- src/pages/admin.tsx
+++ src/pages/admin.tsx
@@ -2,9 +2,9 @@ export default function Home() {
 
     return (
       <>
-        <script src="https://unpkg.com/react@^16/umd/react.production.min.js"></script>
-        <script src="https://unpkg.com/react-dom@^16/umd/react-dom.production.min.js"></script>
-        <script src="https://unpkg.com/netlify-cms-app/dist/netlify-cms-app.js"></script>
+        <script src="https://unpkg.com/react@^16/umd/react.production.min.js" ></script>
+        <script src="https://unpkg.com/react-dom@^16/umd/react-dom.production.min.js" ></script>
+        <script src="https://unpkg.com/netlify-cms-app/dist/netlify-cms-app.js" ></script>
 
         <script id="init">
           NetlifyCmsApp.init();
